2526865f4ca8835a1e8d0303c6daab61018acfb0
ShahidHashmi1 <hashmi.pgh@gmail.com>
2022-12-15 18:12:56 -0500

    resolvers, typeDefs, index, then front end utils for queries and mutations.js

--- server/server.js
+++ server/server.js
@@ -4,12 +4,14 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express')
 const { typeDefs, resolvers } = require('./schemas')
+import { authMiddleware } from './utils/auth';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +23,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
@@ -29,10 +35,10 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Server now running on localhost:${PORT}`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
   })
 })
 
 
-startApolloServer(typeDefs, resolvers);
+startApolloServer(typeDefs, resolvers, context);
